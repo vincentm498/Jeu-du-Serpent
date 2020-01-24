65bc9c2e62f7da244ed00bf1b1f80fdd68b4c15d
Vincent Moreau <vincentm498@gmail.com>
2020-01-24 15:28:30 +0100

    Création du mouvement

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 window.onload = function () {
-    var canvas = document.createElement('canvas');
 
-    canvas.width = 900;
-    canvas.height = 600;
-    canvas.style.border = '1px solid';
-    document.body.appendChild(canvas);
+    var canvas;
+    var ctx;
+    var delay = 100;
+    var xCoord = 0;
+    var yCoord = 0;
 
-    var ctx = canvas.getContext('2d');
-    ctx.fillStyle = "#ff0000";
-    ctx.fillRect(30, 30, 100, 50)
+    init();
+
+    function init() {
+
+        canvas = document.createElement('canvas');
+        canvas.width = 900;
+        canvas.height = 600;
+        canvas.style.border = '1px solid';
+        document.body.appendChild(canvas);
+        ctx = canvas.getContext('2d');
+        refreshCanvas();
+
+    }
+
+    function refreshCanvas() {
+
+        xCoord += 5;
+        yCoord += 5;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = "#ff0000";
+        ctx.fillRect(xCoord, yCoord, 100, 50);
+        setTimeout(refreshCanvas, delay);
+    }
+
+
 }
