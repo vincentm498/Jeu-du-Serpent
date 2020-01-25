4d5b5efaf8434a359c49f1ba7ea091ea79a6a6ce
Vincent Moreau <vincentm498@gmail.com>
2020-01-25 14:02:01 +0100

    Direction du serpernt

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ window.onload = function () {
             [6, 4],
             [5, 4],
             [4, 4]
-        ]);
+        ], "right");
         refreshCanvas();
     }
 
@@ -37,8 +37,9 @@ window.onload = function () {
         ctx.fillRect(x, y, blockSize, blockSize);
     }
 
-    function Snake(body) {
+    function Snake(body, direction) {
         this.body = body;
+        this.direction = direction;
         this.draw = function () {
             ctx.save();
             ctx.fillStyle = "#ff0000";
@@ -49,10 +50,65 @@ window.onload = function () {
         };
         this.advance = function () {
             var nextPosition = this.body[0].slice();
-            nextPosition[0]++;
+            switch (this.direction) {
+                case "left":
+                    nextPosition[0] -= 1;
+                    break;
+                case "right":
+                    nextPosition[0] += 1;
+                    break;
+                case "down":
+                    nextPosition[1] += 1;
+                    break;
+                case "up":
+                    nextPosition[1] -= 1;
+                    break;
+                default:
+                    throw ("direction invalide");
+            };
             this.body.unshift(nextPosition);
             this.body.pop();
+            //console.log("pas de problemes")
+        };
+        this.setDirection = function (newDirection) {
+            var allowedDirections;
+            switch (this.direction) {
+                case "left":
+                case "right":
+                    allowedDirections = ["up", "down"];
+                    break;
+                case "down":
+                case "up":
+                    allowedDirections = ["left", "right"];
+                    break;
+                default:
+                    throw ("direction invalide");
+            };
+            if (allowedDirections.indexOf(newDirection) > -1) {
+                this.direction = newDirection;
+            }
         }
+    }
+    document.onkeydown = function handleKeyDown(e) {
+        var key = e.keyCode;
+        var newDirection;
+        switch (key) {
+            case 37:
+                newDirection = "left"
+                break;
+            case 38:
+                newDirection = "up"
+                break;
+            case 39:
+                newDirection = "right"
+                break;
+            case 40:
+                newDirection = "down"
+                break;
+            default:
+                return;
+        }
+        snakee.setDirection(newDirection);
     }
 
 }
