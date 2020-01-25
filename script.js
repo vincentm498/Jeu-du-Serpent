81d01105cb1edc1f669890a4c83d3919e181b8e4
Vincent Moreau <vincentm498@gmail.com>
2020-01-25 14:12:19 +0100

    Création de la pomme

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ window.onload = function () {
     var ctx;
     var delay = 100;
     var snakee;
+    var applee;
 
     init();
 
@@ -21,6 +22,7 @@ window.onload = function () {
             [5, 4],
             [4, 4]
         ], "right");
+        applee = new Apple([10, 10]);
         refreshCanvas();
     }
 
@@ -28,6 +30,7 @@ window.onload = function () {
         ctx.clearRect(0, 0, canvasWidth, canvasHeight);
         snakee.advance();
         snakee.draw();
+        applee.draw();
         setTimeout(refreshCanvas, delay);
     }
 
@@ -89,6 +92,22 @@ window.onload = function () {
             }
         }
     }
+
+    function Apple(position) {
+        this.position = position;
+        this.draw = function () {
+            ctx.save();
+            ctx.fillStyle = "#33cc33";
+            ctx.beginPath();
+            var radius = blockSize / 2;
+            var x = position[0] * blockSize + radius;
+            var y = position[1] * blockSize + radius;
+            ctx.arc(x, y, radius, 0, Math.PI * 2, true);
+            ctx.fill();
+            ctx.restore();
+        };
+    };
+
     document.onkeydown = function handleKeyDown(e) {
         var key = e.keyCode;
         var newDirection;
